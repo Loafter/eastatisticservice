df87d6b1a5caeb2d23d220387f88302bb4897bd0
Loafter <mopper@yandex.ru>
2015-08-10 20:19:50 +0300

    cosmetic fix

--- EAStatisticWebService/EAStatisticService.cs
+++ EAStatisticWebService/EAStatisticService.cs
@@ -7,22 +7,15 @@ using System.ServiceModel.Web;
 
 namespace WcfJsonRestService
 {
-    public class IntWal
-    {
-        public int v;
-    }
-
     public class ModStatDat
     {
-      public  string name;
-      public IEnumerable data;
+        public string name;
+        public IEnumerable data;
     }
     public class EAStatisticService : IEAStatisticService
     {
-        [WebInvoke(Method = "GET",
-                    ResponseFormat = WebMessageFormat.Json,
-                    UriTemplate = "day_modality.js")]
-        public ModStatDat[] DayModality()
+
+        public void setCors()
         {
             //разрешает доступ для всех доменов; если надо разрешить определенные домены, то передаем их имена через запятую во 2 параметр вместо *
             WebOperationContext.Current.OutgoingResponse.Headers.Add("Access-Control-Allow-Origin", "*");
@@ -30,26 +23,51 @@ namespace WcfJsonRestService
             WebOperationContext.Current.OutgoingResponse.Headers.Add("Access-Control-Allow-Methods", "GET");
             //определяет необходимо ли требовать передачу credentials при выполнении данного метода; в моем случае они не нужны
             WebOperationContext.Current.OutgoingResponse.Headers.Add("Access-Control-Allow-Credentials", "false");
-                    using (var dm = new DCMARCH1Entities())
+        }
+
+        public ModStatDat[] ModalityStat()
+        {
+
+            this.setCors();
+            using (var dm = new DCMARCH1Entities())
             {
                 //.Where(r => r.C00080020.Value.Date == DateTime.Today)
                 var ret = new ModStatDat[2];
-                ret[0]=new ModStatDat()
+                ret[0] = new ModStatDat()
                 {
                     name = "Modality",
                     data = dm.tblDICOMStudy.GroupBy(p => p.C00080061).Select(g => g.Key).ToArray()
                 };
                 ret[1] = new ModStatDat()
                 {
-                    name = "Modality",
-                    data = dm.tblDICOMStudy.GroupBy(p => p.C00080061).Select(g =>  g.Count()).ToArray()
+                    name = "Count",
+                    data = dm.tblDICOMStudy.GroupBy(p => p.C00080061).Select(g => g.Count()).ToArray()
                 };
-
+                return ret;
+            };
+        }
+        public ModStatDat[] DeviceDayStat()
+        {
+            this.setCors();
+            using (var dm = new DCMARCH1Entities())
+            {
+                //.Where(r => r.C00080020.Value.Date == DateTime.Today)
+                var ret = new ModStatDat[2];
+                ret[0] = new ModStatDat()
+                {
+                    name = "Modality",
+                    data = dm.tblDICOMStudy.GroupBy(p => p.C00080061).Select(g => g.Key).ToArray()
+                };
+                ret[1] = new ModStatDat()
+                {
+                    name = "Count",
+                    data = dm.tblDICOMStudy.GroupBy(p => p.C00080061).Select(g => g.Count()).ToArray()
+                };
                 return ret;
             };
         }
     }
- 
+
     public class Person
     {
         public int Id { get; set; }
--- EAStatisticWebService/IEAStatisticService.cs
+++ EAStatisticWebService/IEAStatisticService.cs
@@ -9,6 +9,11 @@ namespace WcfJsonRestService
     public interface IEAStatisticService
     {
         [OperationContract]
-        ModStatDat[] DayModality();
+        [WebInvoke(Method = "GET",ResponseFormat = WebMessageFormat.Json,UriTemplate = "modality_stat.js")]
+        ModStatDat[] ModalityStat();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "day_device_stat.js")]
+        ModStatDat[] DeviceDayStat();
     }
 }
